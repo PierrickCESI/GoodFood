d653a3ad008a90296041d47858abfd0603c053e8
dimitrily8 <36041333+dimitrily8@users.noreply.github.com>
2022-05-09 15:32:42 +0200

    Create footer page
    Création du footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <footer>
+            <div class="row">
+                <div class="container-fluid bg-dark mt-5">
+                    <div class="container-fluid d-flex justify-content-around bg-white mt-2">
+                        <div class="col-2">
+                            <h5>Nos produits</h5>
+                            <h7>La carte</h7><br>
+                            <h7>Nos pizzas</h7><br>
+                            <h7>Nos recettes</h7>
+                        </div>
+
+                        <div class="col-2">
+                            <h5>GoodFood</h5>
+                            <h7>A propos</h7><br>
+                            <h7>Cookies</h7><br>
+                            <h7>Données personnelles</h7><br>
+                            <h7>Newsletter</h7>
+                        </div>
+
+                        <div class="col-2">
+                            <h5>Nous contacter</h5>
+                            <h7>Service client</h7><br>
+                            <h7>Politique divulgation des données</h7><br>
+                            <h7>Désinscrire de la newsletter</h7>
+                        </div>
+
+                        <div class="col-2">
+                            <h5>Conditions des offres</h5>
+                            <h7>Nos promos</h7>
+                        </div>
+                    </div>
+                    <div class="container-fluid d-flex justify-content-around bg-white">
+                        <div class="col-3 mt-5">
+                            <h5>Payez en toutes sécurité avec ...</h5>
+                        </div>
+                        <div class="col-3 mt-5">
+                            <h5>Suivez-nous...</h5>
+                        </div>
+                    </div>
+                    <div class="container-fluid bg-white mb-2 text-center">
+                        <h5>Mentions légales</h5>
+                        <h5>Conditions générales de ventes</h5>
+                        <h5>Paramètres des cookies</h5>
+                        <h5>Pour votre santé, mangez au moins cinq fruits et légumes par jour www.mangerbouger.fr</h5>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
