dcf16323526015c44a0caedc5a4a8c9d4b5aeb17
Pierrick Blondeau <pierrick.blondeau@viacesi.fr>
2022-06-01 09:11:01 +0200

    FIX RegistrationTest

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -20,10 +21,14 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register()
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'name' => 'Test Name User',
+            'firstname' => 'Test Firstname User',
             'email' => 'test@example.com',
+            'email_verified_at' => date(now()),
             'password' => 'password',
             'password_confirmation' => 'password',
+            'typable_type' => 'App\Models\Customer',
+            'typable_id' => User::max('typable_id') + 1,
         ]);
 
         $this->assertAuthenticated();
