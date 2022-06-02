c50ef68f9251c320433893ee861215ce57252893
Pierrick Blondeau <pierrick.blondeau@viacesi.fr>
2022-06-02 11:43:00 +0200

    footer.blade.php lang mapping

---
+++ resources/lang/de/footer.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Carte Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during footer for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'products' => 'Unsere Produkte',
+    'menu' => 'Menü',
+    'pizza' => 'Wagen',
+    'recipe' => 'Unsere Rezepte',
+    'about' => 'Über uns',
+    'cookie' => 'Cookies',
+    'personalData' => 'Persönliche Daten',
+    'newsletter' => 'Newsletter',
+    'contact' => 'Kontakt',
+    'service' => 'Kundenbetreuung',
+    'policy' => 'Politik zur Offenlegung von Daten',
+    'unsubscribe' => 'Den Newsletter abbestellen',
+    'offer' => 'Bedingungen der Angebote',
+    'promotion' => 'Unsere Werbeaktionen',
+    'pay' => 'Sicher bezahlen mit ...',
+    'follow' => 'Folgen Sie uns...',
+    'legal' => 'Rechtliche Informationen',
+    'condition' => 'Allgemeine Verkaufsbedingungen',
+    'cookieParameter' => 'Cookie-Einstellungen',
+    'eatMove' => 'Essen Sie für Ihre Gesundheit mindestens fünf Früchte und Gemüse pro Tag www.mangerbouger.fr',
+
+];
