42d0232241e0ec3ba3caa2ae75b69d6ce4df3841
tomJacquemart <94608060+Motazerty@users.noreply.github.com>
2022-06-02 15:33:15 +0200

    align header fix carroussel

--- resources/views/cmd details.blade.php
+++ resources/views/cmd details.blade.php
@@ -12,6 +12,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,11 +9,11 @@
                 <div class="container-fluid bg-dark">
                     <div class="container-fluid d-flex justify-content-around bg-white mt-2 mb-2 ">
                         <a href="{{route('index')}}"><img src="/img/logo.png" style="width: 47px; height: 57px;"></a>
-                        <a href="{{route('menu')}}"><h3 class="my-auto">{{__('header.menu') }}</h3></a>
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#ModalFormula"><h3 class="my-auto">{{__('header.ourFormulas') }}</h3></a>
-                        <a href="#"><h3 class="my-auto">{{__('header.ourPromotions') }}</h3></a>
-                        <a href="{{route('cmd details')}}"><h3 class="my-auto">{{__('header.shoppingCart') }}</h3></a>
-                        <a href="{{route('login')}}"><h3 class="my-auto">{{__('header.login') }}</h3></a>
+                        <a href="{{route('menu')}}"><h3 class="my-auto" style="padding-top: 10%">{{__('header.menu') }}</h3></a>
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#ModalFormula"><h3 class="my-auto" style="padding-top: 5%">{{__('header.ourFormulas') }}</h3></a>
+                        <a href="#"><h3 class="my-auto" style="padding-top: 4.5%">{{__('header.ourPromotions') }}</h3></a>
+                        <a href="{{route('cmd details')}}"><h3 class="my-auto" style="padding-top: 5%">{{__('header.shoppingCart') }}</h3></a>
+                        <a href="{{route('login')}}"><h3 class="my-auto" style="padding-top: 12.5%">{{__('header.login') }}</h3></a>
                     </div>
                 </div>
 
